2ac748126d153cc3fe6be55062006f5d7b684fba
Rob Landley <rob@landley.net>
2006-11-30 14:58:26 -0500

    Teach the download script to make symlinks, and update kernel to 2.6.19.

--- download.sh
+++ download.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-# This isn't ready yet.
+export SRCDIR=sources/packages
+export STAGE2SRC=../packages
+mkdir -p "$SRCDIR"
 
 function download()
 {
   FILENAME=`echo "$URL" | sed 's .*/  '`
+  BASENAME=`echo "$FILENAME" | sed -r -e 's/-*([0-9\.]|-rc)*(\.tar\..z2*)$/\2/'`
+
+  if [ ! -z "$STAGEDIR" ]
+  then
+    rm -f "$STAGEDIR/$BASENAME" 2> /dev/null
+    ln -s "$STAGE2SRC/$FILENAME" "$STAGEDIR/$BASENAME"
+  fi
 
   # The extra "" is so we test the sha1sum after the last download.
 
@@ -47,27 +56,20 @@ function download()
 echo "=== Download source code." &&
 
 export SRCDIR=sources/packages
-mkdir -p $SRCDIR
+mkdir -p "$SRCDIR"
 
-# Base operating system
+# Required for cross compile toolchain
 
-URL=http://www.kernel.org/pub/linux/kernel/v2.6/testing/linux-2.6.19-rc6.tar.bz2 \
-SHA1=770e825da8ba9884fc4f7ca5fd473c24174365ad \
+export STAGEDIR=sources/build-cross
+mkdir -p "$STAGEDIR"
+
+URL=http://www.kernel.org/pub/linux/kernel/v2.6/linux-2.6.19.tar.bz2 \
+SHA1=c00d5891e2461bce45be51c1ef7846ee842792dd \
 download &&
 
 URL=http://www.uclibc.org/downloads/snapshots/uClibc-20061128.tar.bz2 \
 SHA1=50c024ac137262981348ad54e0f64d83db1bce4e \
 download &&
-
-URL=http://www.busybox.net/downloads/busybox-1.2.2.tar.bz2 \
-SHA1=59670600121c9dacfd61e72e34f4bd975ec2c36f \
-download &&
-
-URL=http://superb-east.dl.sourceforge.net/sourceforge/squashfs/squashfs3.1.tar.gz \
-SHA1=89d537fd18190402ff226ff885ddbc14f6227a9b \
-download &&
-
-# Build tools
 
 URL=ftp://ftp.gnu.org/gnu/binutils/binutils-2.17.tar.bz2 \
 SHA1=a557686eef68362ea31a3aa41ce274e3eeae1ef0 \
@@ -75,6 +77,19 @@ download &&
 
 URL=ftp://ftp.gnu.org/gnu/gcc/gcc-4.1.1/gcc-core-4.1.1.tar.bz2 \
 SHA1=147e12bf96a8d857fda1d43f0d7ea599b89cebf9 \
+download &&
+
+# Required for native build environment
+
+export STAGEDIR=sources/build-native
+mkdir -p "$STAGEDIR"
+
+URL=http://superb-east.dl.sourceforge.net/sourceforge/squashfs/squashfs3.1.tar.gz \
+SHA1=89d537fd18190402ff226ff885ddbc14f6227a9b \
+download &&
+
+URL=http://www.busybox.net/downloads/busybox-1.2.2.tar.bz2 \
+SHA1=59670600121c9dacfd61e72e34f4bd975ec2c36f \
 download &&
 
 URL=ftp://ftp.gnu.org/gnu/make/make-3.81.tar.bz2 \
