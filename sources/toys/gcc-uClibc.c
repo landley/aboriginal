e225ee6b18600c434a616ece4ac562db8f392123
Rob Landley <rob@landley.net>
2006-12-22 13:48:27 -0500

    Weird ordering dependency.  I think it's a uClibc bug?

--- sources/toys/gcc-uClibc.c
+++ sources/toys/gcc-uClibc.c
@@ -11,8 +11,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <string.h>
 #include <strings.h>
+#include <string.h>
 #include <unistd.h>
 #include <errno.h>
 #include <sys/stat.h>
