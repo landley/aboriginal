7ff91ee03ed4e77a32649b2f387da46487912201
Rob Landley <rob@landley.net>
2007-02-14 16:29:20 -0500

    Go back to a sane build wrapper.  Being clever doesn't help here.

--- build.sh
+++ build.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-source include.sh
-
-# I have no idea why this is spawning a subshell.  I want "thing || exit | tee"
-# but there doesn't seem to be a syntax for that, so we remember PID and kill.
-
-PARENT=$$
-{
-  ./download.sh || kill $PARENT
-  ./host-tools.sh || kill $PARENT
-} 2>&1 | tee out-all.txt
-
-for i in "$@"
-do
-  {
-    ./cross-compiler.sh "$i" || kill $PARENT
-    ./mini-native.sh "$i" || kill $PARENT
-  } 2>&1 | tee "out-$i.txt"
-done
+./download.sh &&
+./host-tools.sh &&
+./cross-compiler.sh $1 &&
+./mini-native.sh $1
