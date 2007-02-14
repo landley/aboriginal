17c80d45718c39c590d16eb123b754955c1704ff
Rob Landley <rob@landley.net>
2007-02-14 16:30:12 -0500

    Move to gcc 4.1.2 and update uClibc snapshot.

--- download.sh
+++ download.sh
@@ -16,7 +16,7 @@ URL=http://www.kernel.org/pub/linux/kernel/v2.6/linux-2.6.20.tar.bz2 \
 SHA1=e730979adfb287528263e779c91284659672510d \
 download &&
 
-URL=http://www.uclibc.org/downloads/snapshots/uClibc-20070130.tar.bz2 \
+URL=http://www.uclibc.org/downloads/snapshots/uClibc-20070214.tar.bz2 \
 SHA1= \
 download &&
 
@@ -24,8 +24,12 @@ URL=ftp://ftp.gnu.org/gnu/binutils/binutils-2.17.tar.bz2 \
 SHA1=a557686eef68362ea31a3aa41ce274e3eeae1ef0 \
 download &&
 
-URL=ftp://ftp.gnu.org/gnu/gcc/gcc-4.1.1/gcc-core-4.1.1.tar.bz2 \
-SHA1=147e12bf96a8d857fda1d43f0d7ea599b89cebf9 \
+URL=ftp://ftp.gnu.org/gnu/gcc/gcc-4.1.2/gcc-core-4.1.2.tar.bz2 \
+SHA1=d6875295f6df1bec4a6f4ab8f0da54bfb8d97306 \
+download &&
+
+URL=http://ftp.gnu.org/gnu/gcc/gcc-4.1.2/gcc-g++-4.1.2.tar.bz2 \
+SHA1=e29c6e151050f8b5ac5d680b99483df522606143 \
 download &&
 
 URL=http://landley.net/code/toybox/downloads/toybox-0.0.3.tar.bz2 \
@@ -34,15 +38,11 @@ download &&
 
 # Ye olde emulator
 
-URL=http://qemu.org/qemu-0.8.2.tar.gz \
-SHA1=d775239a545e7d0c5b677660a2122df590afde6d \
+URL=http://qemu.org/qemu-0.9.0.tar.gz \
+SHA1=1e57e48a06eb8729913d92601000466eecef06cb \
 download &&
 
 # Required for native build environment
-
-URL=http://ftp.gnu.org/gnu/gcc/gcc-4.1.1/gcc-g++-4.1.1.tar.bz2 \
-SHA1=909672543af9a7b88898cd256e25ccd4a9f009b1 \
-download &&
 
 URL=http://superb-east.dl.sourceforge.net/sourceforge/squashfs/squashfs3.1.tar.gz \
 SHA1=89d537fd18190402ff226ff885ddbc14f6227a9b \
@@ -61,7 +61,7 @@ SHA1=b3e158877f94e66ec1c8ef604e994851ee388b09 \
 download &&
 
 URL=http://superb-east.dl.sourceforge.net/sourceforge/strace/strace-4.5.14.tar.bz2 \
-SHA1= \
+SHA1=72c17d1dd6786d22ca0aaaa7292b8edcd70a27de \
 download &&
 
 echo === Got all source. &&
