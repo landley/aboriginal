5403e472925b1b4c0a1602760911d6db8516e9b5
Rob Landley <rob@landley.net>
2007-06-26 22:10:12 -0400

    Add time information to the build output.

--- build.sh
+++ build.sh
@@ -12,14 +12,14 @@ fi
 # Download source code and build host tools.
 
 ./download.sh || exit 1
-./host-tools.sh || exit 1
+/usr/bin/time ./host-tools.sh || exit 1
 
 # Run the steps in order for each architecture listed on the command line
 for i in "$@"
 do
   echo "=== Building ARCH $i"
-  ./cross-compiler.sh $i || exit 1
+  /usr/bin/time ./cross-compiler.sh $i || exit 1
   echo "=== native ($i)"
-  ./mini-native.sh $i || exit 1
-  ./package-mini-native.sh $i || exit 1
+  /usr/bin/time ./mini-native.sh $i || exit 1
+  /usr/bin/time ./package-mini-native.sh $i || exit 1
 done
