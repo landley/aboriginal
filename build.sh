1b7ba793e23245a961a0ab522b6c7fd8d6a7a5e2
Rob Landley <rob@landley.net>
2007-06-29 10:19:04 -0400

    Use shell builtin time instead of /usr/bin version not everybody has. (they behave differently, see "/usr/bin/time exit 1" vs "time exit 1", but the shell version's good enough for now.)

--- build.sh
+++ build.sh
@@ -12,14 +12,14 @@ fi
 # Download source code and build host tools.
 
 ./download.sh || exit 1
-/usr/bin/time ./host-tools.sh || exit 1
+time ./host-tools.sh || exit 1
 
 # Run the steps in order for each architecture listed on the command line
 for i in "$@"
 do
   echo "=== Building ARCH $i"
-  /usr/bin/time ./cross-compiler.sh $i || exit 1
+  time ./cross-compiler.sh $i || exit 1
   echo "=== native ($i)"
-  /usr/bin/time ./mini-native.sh $i || exit 1
-  /usr/bin/time ./package-mini-native.sh $i || exit 1
+  time ./mini-native.sh $i || exit 1
+  time ./package-mini-native.sh $i || exit 1
 done
