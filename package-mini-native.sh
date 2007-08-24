d61ccca8caf40edf9170db350b8bee8715aeb011
Rob Landley <rob@landley.net>
2007-08-23 20:52:11 -0500

    For some reason, ubuntu is only creating /dev/loop0 right now.  I have no idea where this bug came from.  (Is it trying to create them dynamically?)

--- package-mini-native.sh
+++ package-mini-native.sh
@@ -10,7 +10,7 @@ source include.sh
 
 # A 256 meg sparse image
 rm -f "$IMAGE"
-dd if=/dev/zero of="$IMAGE" bs=1024 seek=$[256*1024-1] count=1 &&
+dd if=/dev/zero of="$IMAGE" bs=1024 seek=$[2048*1024-1] count=1 &&
 /sbin/mke2fs -b 1024 -F "$IMAGE" &&
 
 # User User Mode Linux to package this, until toybox mke2fs is ready.
@@ -19,17 +19,17 @@ dd if=/dev/zero of="$IMAGE" bs=1024 seek=$[256*1024-1] count=1 &&
 cat > "${WORK}/uml-package.sh" << EOF &&
 #!/bin/sh
 mount -n -t ramfs /dev /dev
-mknod /dev/loop1 b 7 1
+mknod /dev/loop0 b 7 1
 # Jump to build dir
 echo copying files...
 cd "$BUILD"
-/sbin/losetup /dev/loop1 "$IMAGE"
-mount -n -t ext2 /dev/loop1 "$WORK"
+/sbin/losetup /dev/loop0 "$IMAGE"
+mount -n -t ext2 /dev/loop0 "$WORK"
 tar cC "$NATIVE" tools | tar xC "$WORK"
 mkdir "$WORK"/dev
 mknod "$WORK"/dev/console c 5 1
 umount "$WORK"
-/sbin/losetup -d /dev/loop1
+/sbin/losetup -d /dev/loop0
 umount /dev
 sync
 EOF
