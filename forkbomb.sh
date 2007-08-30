060aece6fdff53b505c46f3e2968ac11dc677ff0
Rob Landley <rob@landley.net>
2007-08-29 20:45:34 -0500

    Of course I'd find a way to make 2 gigs of memory in my new laptop insufficient.

---
+++ forkbomb.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Test script to build every target architecture, logging output.
+#  With --fork, it builds them in parallel
+#  With --nofork, it build them sequentially
+#  With --watch, it displays output from an existing parallel build
+
+function buildarch()
+{
+  nice -n 20 ./cross-compiler.sh $1 &&
+  nice -n 20 ./mini-native.sh $1 &&
+  nice -n 20 ./package-mini-native.sh $1
+}
+
+if [ "$1" != "--watch" ]
+then
+  if [ $# -ne 0 ]
+  then
+    (nice -n 20 ./download.sh && nice -n 20 ./host-tools.sh) || exit 1
+  fi
+  for i in `cd sources/configs; ls`
+  do
+    if [ "$1" == "--nofork" ]
+    then
+      buildarch $i 2>&1 | tee out-$i.txt || exit 1
+    elif [ "$1" == "--fork" ]
+    then
+      (buildarch $i > out-$i.txt 2>&1 &)&
+    else
+      echo "Usage: forkbomb.sh [--fork] [--nofork] [--watch]"
+      exit 1
+    fi
+  done
+fi
+watch -n 3 'for i in *.txt; do /bin/echo -e "\n$i"; tail -n 1 $i; done'
