496f5a38f0948c167208f682adc68ef053ec2d6c
Rob Landley <rob@landley.net>
2007-09-06 04:03:04 -0500

    Minor display improvement.

--- forkbomb.sh
+++ forkbomb.sh
@@ -32,4 +32,4 @@ then
     fi
   done
 fi
-watch -n 3 'for i in *.txt; do /bin/echo -e "\n$i"; tail -n 1 $i; done'
+watch -n 3 'X=; for i in *.txt; do /bin/echo -e "$X$i"; X="\n"; tail -n 1 $i; done'
