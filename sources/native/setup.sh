6cb7991ac0fa04c31a48d36472a9631f40defb2e
Rob Landley <rob@landley.net>
2008-02-21 21:11:01 -0600

    If a second block device exists, mount it on /home.

--- sources/native/setup.sh
+++ sources/native/setup.sh
@@ -17,7 +17,20 @@ mount -t proc /proc /proc
 echo "nameserver 10.0.2.3" > /etc/resolv.conf
 ifconfig eth0 10.0.2.15
 route add default gw 10.0.2.2
+
+# If we have no local clock, you can do this instead:
 #rdate time-b.nist.gov
+
+# If there's a /dev/hdb or /dev/sdb, mount it on home
+
+[ -b /dev/hdb ] && HOMEDEV=/dev/hdb
+[ -b /dev/sdb ] && HOMEDEV=/dev/hdb
+if [ ! -z "$HOMEDEV" ]
+then
+  mkdir -p /home
+  mount $HOMEDEV /home
+  export HOME=/home
+fi
 
 exec /tools/bin/busybox ash
 
