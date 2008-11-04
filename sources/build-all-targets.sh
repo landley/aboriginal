7c7138c8e70d65a6c4093721d8b56c08fac2e465
Rob Landley <rob@landley.net>
2008-11-04 03:57:54 -0600

    Add --fork option to build-all-targets.sh (with --fork 3).  Remove old forkbomb.sh.

--- sources/build-all-targets.sh
+++ sources/build-all-targets.sh
@@ -5,8 +5,7 @@
 # TODO:
 #
 # Wrapper must set:
-# SERVER=busybox.net
-# SERVERDIR=public_html/fwlnew
+# UPLOAD_TO=busybox.net:public_html/fwlnew
 # UNSTABLE=busybox,toybox,uClibc
 
 source sources/functions.sh
@@ -41,50 +40,69 @@ Built on $(date +%F) from:
   Build script:
     Firmware Linux (http://landley.net/code/firmware) mercurial changeset $FWL_REV
 
-  Packages:
+  Base packages:
     uClibc (http://uclibc.org) $(identify_release uClibc)
     BusyBox (http://busybox.net) $(identify_release busybox)
     Linux (http://kernel.org/pub/linux/kernel) $(identify_release linux)
+
+  Toolchain packages:
     Binutils (http://www.gnu.org/software/binutils/) $(identify_release binutils)
     GCC (http://gcc.gnu.org) $(identify_release gcc-core)
     gmake (http://www.gnu.org/software/make) $(identify_release make)
-    Toybox (http://landley.net/code/toybox) $(identify_release toybox)
+    bash (ftp://ftp.gnu.org/gnu/bash) $(identify_release bash)
 
+  Optional packages:
+    Toybox (http://landley.net/code/toybox) $(identify_release toybox)
+    distcc (http://distcc.samba.org) $(identify_release distcc)
+    uClibc++ (http://cxx.uclibc.org) $(identify_release uClibc++)
 EOF
 }
 
 function build_this_target()
 {
-  ./cross-compiler.sh $i || return
+  ./cross-compiler.sh $1 || dienow
   [ ! -z "$SERVER" ] &&
-    scp build/cross-compiler-$i.tar.bz2 ${SERVER}:${SERVERDIR} >/dev/null &
-  ./mini-native.sh $i || return
+    scp build/cross-compiler-$1.tar.bz2 ${SERVER}:${SERVERDIR} >/dev/null &
+  ./mini-native.sh $1 || dienow
   [ ! -z "$SERVER" ] &&
-    scp build/mini-native-$i.tar.bz2 ${SERVER}:${SERVERDIR} >/dev/null &
-  ./package-mini-native.sh $i || return
+    scp build/mini-native-$1.tar.bz2 ${SERVER}:${SERVERDIR} >/dev/null &
+  ./package-mini-native.sh $1 || dienonw
   [ ! -z "$SERVER" ] &&
-    scp build/system-image-$i.tar.bz2 ${SERVER}:${SERVERDIR} >/dev/null &
+    scp build/system-image-$1.tar.bz2 ${SERVER}:${SERVERDIR} >/dev/null &
 }
 
-# Clean up old builds
+function build_log_upload()
+{
+  build_this_target $1 2>&1 | tee out-$1.txt
+  [ ! -z "$SERVER" ] && (cat out-$1 | bzip2 | ssh ${SERVER} \
+    "cat > ${SERVERDIR}/buildlog-$(echo $1 | sed 's/^out-//').bz2") &
+}
 
-hg pull -u &
+# Clean up old builds, fesh fresh packages.
+
+(hg pull -u; ./download.sh || dienow) &
 rm -rf build out-*.txt &
 wait4background 0
 
-# Fetch fresh packages, build host tools, extract packages. 
+# Build host tools, extract packages. 
 
-(./download.sh &&
- ./host-tools.sh &&
- ./download.sh --extract || dienow) | tee out.txt
+(./host-tools.sh && ./download.sh --extract || dienow) | tee out.txt
+
+SERVER="$(echo "$UPLOAD_TO" | sed 's/:.*//')"
+SERVERDIR="$(echo "$UPLOAD_TO" | sed 's/[^:]*://')"
 
 do_readme | tee build/README.txt | ( [ -z "$SERVER" ] && cat || ssh ${SERVER} "cd ${SERVERDIR}; cat > README.txt" ) &
 
 for i in $(cd sources/targets; ls);
 do
-  build_this_target 2>&1 | tee out-$i.txt
-  [ ! -z "$SERVER" ] && (cat out-$i | bzip2 | ssh ${SERVER} \
-    "cat > ${SERVERDIR}/buildlog-$(echo $i | sed 's/^out-//').bz2") &
+  if [ "$1" == "--fork" ]
+  then
+    echo Launching $i
+    (build_log_upload $i 2>&1 </dev/null | grep ===) &
+    [ ! -z "$2" ] && wait4background $[${2}-1]
+  else
+    build_log_upload $i
+  fi
 done
 
 # Wait for ssh/scp invocations to finish.
