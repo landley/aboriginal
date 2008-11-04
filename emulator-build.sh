095797e0c4e53362ee4353c55816136bbaf3578f
Rob Landley <rob@landley.net>
2008-11-03 21:00:46 -0600

    Teach emulator-build.sh to fsck the root filesystem before running it.

--- emulator-build.sh
+++ emulator-build.sh
@@ -4,5 +4,11 @@ SAVEPATH="$PATH"
 source include.sh
 PATH="$SAVEPATH"
 
-cd "${BUILD}/system-image-$ARCH" &&
+cd "${BUILD}/system-image-$ARCH" || exit 1
+
+# A little paranoia.
+fsck.ext2 -y "image-${ARCH}.ext2" </dev/null
+
+# And run it, using the distccd we built (if necessary) and the cross-compiler.
+
 PATH="$HOSTTOOLS:$PATH" ./run-with-distcc.sh "$CROSS"
