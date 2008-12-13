dd35ee137aec4c56c07cc18a97f8e91648035acf
Rob Landley <rob@landley.net>
2008-12-12 20:50:19 -0600

    Provide more info on error exit.

--- sources/functions.sh
+++ sources/functions.sh
@@ -235,7 +235,7 @@ function cleanup_oldfiles()
 
 function actually_dienow()
 {
-  echo -e "\e[31mExiting due to errors\e[0m"
+  echo -e "\e[31mExiting due to errors ($ARCH_NAME $STAGE_NAME $PACKAGE)\e[0m"
   exit 1
 }
 
