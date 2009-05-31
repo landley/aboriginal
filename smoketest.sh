c031e23734233d1fc9f3f76264739fd419a76c80
Rob Landley <rob@landley.net>
2009-05-31 02:36:22 -0500

    Add a 60 second timeout to smoketest.  (Some qemu failure modes involve hanging.)

--- smoketest.sh
+++ smoketest.sh
@@ -3,6 +3,19 @@
 # This script compiles stuff under the final system, using distcc to call out
 # to the cross compiler.
 
+# Set up a timeout.  If it doesn't complete in 60 seconds, it failed.
+
+timeout()
+{
+  sleep 60
+  kill $1
+}
+
+timeout $$ &
+trap "kill $(jobs -p) 2>/dev/null" EXIT
+
+# Call run-from-build with a here document to do stuff.
+
 # Note that the first line of the script is a few spaces followed by a comment
 # character.  This gives some harmless data for the linux boot process to
 # consume and discard before it gets to the command prompt.  I don't know why
