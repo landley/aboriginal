11f7e046d5f7a3dcdc16e40527ad4be133a36418
Rob Landley <rob@landley.net>
2009-06-23 19:38:24 -0500

    Add --logs option to smoketest-all.sh to read existing logs from buildall.sh.

--- smoketest-all.sh
+++ smoketest-all.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [ "$1" == "--logs" ]
+then
+  for i in build/smoketest-*.txt
+  do
+    NAME="$(echo $i | sed 's/smoketest-\(.*\)\.txt/\1/')"
+    echo -n "Testing $NAME:"
+    RESULT="$(grep 'Hello world!' "$i")"
+    [ -z "$RESULT" ] && echo "FAIL" || echo "PASS"
+  done
+
+  exit
+fi
+
 # Test all targets to see whether or not they can compile and run "hello world"
 
 ls -pd build/system-image-* | sed -n 's@.*/system-image-\(.*\)/@\1@p' | while read i
