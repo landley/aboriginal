44d98b0a7143e3847b8ae1eb3a969f1eefa0787a
Rob Landley <rob@landley.net>
2009-07-03 20:06:23 -0500

    Allow specifying package list for nightly.sh, add explicit "fwl" and "all" stages, better handling of triage directories, do smoketest-all in parallel.

--- sources/toys/nightly.sh
+++ sources/toys/nightly.sh
@@ -6,6 +6,8 @@
 TOP="$(pwd)"
 SNAPSHOT_DATE=$(date +"%Y-%m-%d")
 
+TEMPDIR="$TOP"
+
 rm -rf triage.* build &
 
 # Update the scripts, but revert repository back to last release for the
@@ -21,19 +23,31 @@ hg update "$FWL_STABLE"
 # Update each package from repository, generate alt-tarball, and build with
 # that package.
 
-for PACKAGE in busybox uClibc linux ""
+[ -z "$PACKAGES" ] && PACKAGES="busybox uClibc linux fwl all"
+for PACKAGE in $PACKAGES
 do
-  # Update package from repository
-
   export USE_UNSTABLE="$PACKAGE"
-  if [ -z "$PACKAGE" ]
+
+  # Handle special package names "fwl" and "all"
+
+  if [ "$PACKAGE" == "fwl" ]
+  then
+    echo updating build scripts
+    hg update tip
+    USE_UNSTABLE=
+
+  # Note that building the other packages is what updates them,
+  # this just selects and compiles them.
+
+  elif [ "$PACKAGE" == "all" ]
   then
     USE_UNSTABLE=busybox,uClibc,linux
-    PACKAGE=all
-    hg update tip
+
+  # Update package from repository
+
   else
     cd "$TOP/../$PACKAGE"
-    echo pulling "$PACKAGE"
+    echo updating "$PACKAGE"
     git pull
     git archive master --prefix=$PACKAGE/ | bzip2 > \
       "$TOP"/packages/alt-$PACKAGE-0.tar.bz2
@@ -45,13 +59,13 @@ do
   cd "$TOP"
   FORK=1 CROSS_COMPILERS_EH=i686 NATIVE_COMPILERS_EH=1 nice -n 20 ./buildall.sh
 
-  ./smoketest-all.sh --logs > build/status.txt
+  FORK=1 ./smoketest-all.sh --logs > build/status.txt
 
   DESTDIR="$TOP/../snapshots/$PACKAGE/$SNAPSHOT_DATE"
   rm -rf "$DESTDIR"
   mkdir -p "$DESTDIR"
   mv build/logs build/*.tar.bz2 "$DESTDIR"
-  mv build triage.$PACKAGE
+  mv build "$TEMPDIR/triage.$PACKAGE"
 done
 
 # Upload stuff
