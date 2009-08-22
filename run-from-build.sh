d5449ec7f5a4601a9ebee7ee3a724456bd893894
Rob Landley <rob@landley.net>
2009-08-21 23:21:57 -0500

    Comment tweaks, better document what it's doing.

--- run-from-build.sh
+++ run-from-build.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-source sources/include.sh || exit 1
+# Read the configuration files for this target.
 
+source sources/include.sh || exit 1
 read_arch_dir "$1"
 
-SYSDIR="${BUILD}/system-image-$ARCH_NAME"
+# Fail if this target hasn't been built yet.
 
+SYSDIR="${BUILD}/system-image-$ARCH_NAME"
 if [ ! -f "$SYSDIR/run-emulator.sh" ]
 then
   [ -z "$FAIL_QUIET" ] && echo "No $SYSDIR/run-emulator.sh" >&2
   exit 1
 fi
-
 cd "$SYSDIR" || exit 1
 
-[ -z "$SKIP_HOME" ] && [ -z "$MAKE_HDB" ] && MAKE_HDB="--make-hdb 2048"
+# Should we create a 2 gigabyte /dev/hdb image to provide the emulator with
+# some writable scratch space?  (If one already exists, fsck it.)  This
+# image (if it exists) will be mounted on /home by the emulated system's
+# init script.
 
-# A little paranoia.
+[ -z "$SKIP_HOME" ] && [ -z "$MAKE_HDB" ] && MAKE_HDB="--make-hdb 2048"
 [ -f "image-${ARCH}.ext2" ] && fsck.ext2 -y "image-${ARCH}.ext2" </dev/null
 
-# And run it, using the distccd we built (if necessary) and the cross-compiler.
+# Run the emulator, using the distccd we built (if necessary) to dial out
+# to the cross-compiler.  If emulator is killed, take down distccd processes
+# as well.
 
 trap "killtree $$" EXIT
 
