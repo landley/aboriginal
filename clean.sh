6e9d0dd843ea02abbdffc5889951788b49c61c27
Rob Landley <rob@landley.net>
2009-11-15 14:50:25 -0600

    Add a clean.sh script, to "rm -rf build/*-*".
    What it does is fairly trivial, but not necessarily obvious, and it's an
    excuse to add a big comment explaining things.

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Delete all the target stages, to force them to rebuild next time build.sh
+# runs.
+
+# This leaves build.packages and build/host alone.  (You can delete those
+# too if you like, rm -rf build is safe, it just means ./download.sh --extract
+# and ./host-tools.sh will have to do their thing again, which takes a while.)
+
+rm -rf build/*-*
