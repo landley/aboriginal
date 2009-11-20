e58dda324747d8cc4f76e794994ea157cfdfbad9
Rob Landley <rob@landley.net>
2009-11-19 19:38:04 -0600

    Change shell prompt in system image to start with ($HOST)

--- sources/native/sbin/init.sh
+++ sources/native/sbin/init.sh
@@ -14,6 +14,8 @@ mdev -s
 # Make sure /proc is there
 mountpoint -q proc || mount -t proc proc proc
 
+export PS1='($HOST) \w \$ '
+
 # If we're running under qemu, do some more setup
 if [ $$ -eq 1 ]
 then
