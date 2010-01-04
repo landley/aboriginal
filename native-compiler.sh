b9a2df19e46add21ab6220dc77f78545643595ba
Rob Landley <rob@landley.net>
2010-01-03 22:34:06 -0600

    Helps to actually remember to add the new file.

---
+++ native-compiler.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Build a compiler for a given target
+
+source sources/include.sh || exit 1
+read_arch_dir "$1"
+check_for_base_arch || exit 0
+check_prerequisite "${ARCH}-cc"
+check_prerequisite "${FROM_ARCH}-cc"
+
+blank_tempdir "$STAGE_DIR"
+mkdir -p "$STAGE_DIR/bin" || dienow
+
+# Build C Library
+
+build_section linux-headers
+build_section uClibc
+
+# Build binutils, gcc, and ccwrap
+
+build_section binutils
+build_section gcc
+build_section ccwrap
+
+# Tell future packages to link against the libraries in the new root filesystem,
+# rather than the ones in the cross compiler directory.
+
+export "$(echo $ARCH | sed 's/-/_/g')"_CCWRAP_TOPDIR="$STAGE_DIR"
+
+build_section uClibc++
+
+# Delete some unneeded files
+
+[ -z "$SKIP_STRIP" ] && rm -rf "$STAGE_DIR"/{info,man}
+
+create_stage_tarball
