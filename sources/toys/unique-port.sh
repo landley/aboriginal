360ca9e15da990a09d3a4d4ed87d2cb42e98b244
Rob Landley <rob@landley.net>
2010-08-05 13:10:07 -0500

    If some clever sysadmin claimed 1024-65535 for local ports, punt.

--- sources/toys/unique-port.sh
+++ sources/toys/unique-port.sh
@@ -4,10 +4,15 @@ unique_port()
 
   START=8192
   RANGE=$[$(awk '{print $1}' /proc/sys/net/ipv4/ip_local_port_range)-$START]
-  if [ $RANGE -lt 1 ]
+  if [ $RANGE -lt 8 ]
   then
     START=$[$(awk '{print $2}' /proc/sys/net/ipv4/ip_local_port_range)]
     RANGE=$[65535-$START]
+    if [ $RANGE -lt 8 ]
+    then
+      START=16384
+      RANGE=32768
+    fi
   fi
   echo $[($$%$RANGE)+$START]
 }
