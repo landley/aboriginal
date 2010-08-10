6254af292c276adb981dc5e43a49a0ad271d49b8
Rob Landley <rob@landley.net>
2010-08-10 00:17:48 -0500

    Remove script that moved to "more" directory.

--- run-from-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run development environment out of build directory, using host-tools.sh if
-# available.
-
-cd build/system-image-"$1" &&
-  PATH="$(pwd)/build/host:$PATH" ./dev-environment.sh
