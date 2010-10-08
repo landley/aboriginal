ff679dba50dec1a63d5ea40d938ae6faee786331
Rob Landley <rob@landley.net>
2010-10-07 23:55:46 -0500

    Teach build-control-images.sh to call the new subdirectory format builds (with integrated patch directories and file trees and such).

--- more/build-control-images.sh
+++ more/build-control-images.sh
@@ -9,9 +9,9 @@ blank_tempdir build/control-images
 # Iterate through sources/native-builds and run each script, writing output
 # to build/control-images/$SCRIPTNAME.hdc
 
-for i in sources/native-builds/*.sh
+for i in sources/native-builds/{*.sh,*/make-control-image.sh}
 do
-  SCRIPTNAME=$(echo $i | sed 's@.*/\(.*\)\.sh@\1@')
+  SCRIPTNAME=$(echo $i | sed 's@sources/native-builds/\([^./]*\).*@\1@')
   # Forking doesn't work yet due to extract collisions with the same package
   #maybe_fork "$i build/control-images/$SCRIPTNAME.hdc | maybe_quiet"
   $i build/control-images/$SCRIPTNAME.hdc | maybe_quiet
