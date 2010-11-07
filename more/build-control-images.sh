a120107b41980e0859508b567cfaed6f4c60e729
Rob Landley <rob@landley.net>
2010-11-07 15:57:32 -0600

    Missed a bit of the name change.

--- more/build-control-images.sh
+++ more/build-control-images.sh
@@ -6,12 +6,12 @@
 
 blank_tempdir build/control-images
 
-# Iterate through sources/native-builds and run each script, writing output
+# Iterate through sources/control-images and run each script, writing output
 # to build/control-images/$SCRIPTNAME.hdc
 
-for i in sources/native-builds/{*.sh,*/make-control-image.sh}
+for i in sources/control-images/{*.sh,*/make-control-image.sh}
 do
-  SCRIPTNAME=$(echo $i | sed 's@sources/native-builds/\([^./]*\).*@\1@')
+  SCRIPTNAME=$(echo $i | sed 's@sources/control-images/\([^./]*\).*@\1@')
   # Forking doesn't work yet due to extract collisions with the same package
   #maybe_fork "$i build/control-images/$SCRIPTNAME.hdc | maybe_quiet"
   $i build/control-images/$SCRIPTNAME.hdc | maybe_quiet
