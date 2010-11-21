43fdd2e34a23b571524e42a0cc31f32660099860
Rob Landley <rob@landley.net>
2010-11-21 15:02:04 -0600

    The LFS iproute2 build gets confused because "find -type f" in a directory created with "cp -s" skips symlinks and thus never finds anything.  Use sed to add a -L to all those find invocations, to follow symlinks.

--- sources/control-images/lfs-bootstrap/mnt/build/iproute2.sh
+++ sources/control-images/lfs-bootstrap/mnt/build/iproute2.sh
@@ -6,6 +6,9 @@ sed -i '/^TARGETS/s@arpd@@g' misc/Makefile &&
 # bugfix, why this isn't a patch I have no idea.
 sed -i '1289i\\tfilter.cloned = 2;' ip/iproute.c &&
 
+# Don't be confused by symlinks
+sed -i 's/ find / find -L /g' Makefile &&
+
 make DESTDIR= -j $CPUS &&
 make DESTDIR= SBINDIR=/sbin MANDIR=/usr/share/man \
   DOCDIR=/usr/share/doc/iproute2 install
