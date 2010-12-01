e708f8f769ee623d399ee5f358789f22681f432c
Rob Landley <rob@landley.net>
2010-12-01 17:57:46 -0600

    Add script.nolink option for packages that really don't like the whole 'tree of symlinks' thing.  Migrate vim and remove the -L hack.

--- sources/control-images/bootstrap-skeleton/mnt/build-one-package.sh
+++ sources/control-images/bootstrap-skeleton/mnt/build-one-package.sh
@@ -10,14 +10,22 @@ source /mnt/functions.sh || exit 1
 
 set_titlebar "$1"
 
-if [ -d "/mnt/packages/$1" ]
-then
+# Three types of /mnt/build scripts:
+#
+# $1.nosrc - no source code in /mnt/build, perform no setup/cleanup.
+# $1.sh - source is symlink tree to "/mnt/build/$1".  This is faster and uses
+#         less disk space but confuses some packages.
+# $1.nolink - source is conventional copy of /mnt/build/$1
 
-  # Snapshot source
+if [ ! -e "/mnt/build/$1.nosrc" ]
+then
+  [ -e "/mnt/build/$1.nolink" ] && LINKTYPE=dp || LINKTYPE=s
+
+  # Snapshot source from /mnt/build/$1 to /home/$1
 
   cd /home &&
   rm -rf "/home/$1" &&
-  cp -sfR "/mnt/packages/$1" "$1" &&
+  cp -${LINKTYPE}fR "/mnt/packages/$1" "$1" &&
   cd "$1" || exit 1
 
   # Lobotomize config.guess so it won't complain about unknown target types.
@@ -39,7 +47,7 @@ fi
 # Call package build script
 
 mkdir -p /home/log
-time "/mnt/build/${1}.$EXT" 2>&1 | tee "/home/log/$1.log"
+time "/mnt/build/$1".* 2>&1 | tee "/home/log/$1.log"
 if [ $? -ne 0 ]
 then
   echo "$1" died >&2
@@ -48,7 +56,7 @@ fi
 
 # Delete copy of source if build succeeded
 
-if [ -d "/mnt/packages/$1" ]
+if [ ! -z "$LINKTYPE" ]
 then
   cd /home &&
   rm -rf "$1" &&
