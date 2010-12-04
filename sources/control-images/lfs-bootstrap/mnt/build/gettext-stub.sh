54ae2abc5da59ec2784f86ba159863449177104c
Rob Landley <rob@landley.net>
2010-12-03 21:47:10 -0600

    Need -fpic to build a shared libarary.

--- sources/control-images/lfs-bootstrap/mnt/build/gettext-stub.sh
+++ sources/control-images/lfs-bootstrap/mnt/build/gettext-stub.sh
@@ -2,6 +2,6 @@
 
 # Stub to compile packages that refuse to build without gettext.
 
-gcc -shared -o /usr/lib/libintl.so libintl-stub.c &&
+gcc -shared -fpic -o /usr/lib/libintl.so libintl-stub.c &&
 cp libintl-stub.h /usr/include/libintl.h &&
 cp msgfmt /usr/bin
