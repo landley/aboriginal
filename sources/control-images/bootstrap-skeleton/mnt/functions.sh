e8188686faaf841dd33a01922efe556252290a95
Rob Landley <rob@landley.net>
2011-04-13 03:07:02 -0500

    Move do_in_chroot in control-images to setup-choot in system-image, and move zapchroot into system-image as well.  New setup-chroot is more flexible, copying whatever existing host mounts you've got and with the ability to re-enter an existing chroot (if the target directory exists it'll just setup and tear down the bind mounts again without copying files).

--- sources/control-images/bootstrap-skeleton/mnt/functions.sh
+++ sources/control-images/bootstrap-skeleton/mnt/functions.sh
@@ -15,26 +15,3 @@ dotprogress()
 {
   while read i; do echo -n .; done; echo
 }
-
-do_in_chroot()
-{
-  # Copy root filesystem into a new chroot directory and restart in there.
-
-  CHROOT="$1"
-  shift
-
-  set_titlebar "Setup chroot"
-  mkdir "$CHROOT"
-  cp -a /mnt/files/. "$CHROOT"
-  find / -xdev | cpio -m -v -p "$CHROOT" | dotprogress
-  for i in mnt proc sys dev; do mount --bind /$i "$CHROOT"/$i; done
-
-  echo Chroot
-  chroot "$CHROOT" "$@"
-  RC=$?
-
-  echo Chroot cleanup
-  for i in mnt proc sys dev; do umount "$CHROOT"/$i; done
-
-  return $RC
-}
