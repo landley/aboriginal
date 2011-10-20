5feb09a9bbcc197064bd60a79464e22cb9ed0f02
Rob Landley <rob@landley.net>
2011-10-20 02:03:48 -0500

    Oops, the SYSIMAGE_TYPE=ext2 SYSIMAGE_HDA_MEGS stuff never quite got completed when root-image was split out from system-image.  Make that work.

--- root-image.sh
+++ root-image.sh
@@ -55,31 +55,27 @@ then
 
   [ -z "$SYSIMAGE_HDA_MEGS" ] && SYSIMAGE_HDA_MEGS=64
 
-  echo "/dev d 755 0 0 - - - - -" > "$WORK/devs" &&
-  echo "/dev/console c 640 0 0 5 1 0 0 -" >> "$WORK/devs" &&
-
   # Produce a filesystem with the currently used space plus 20% for filesystem
   # overhead, which should always be big enough.
 
   BLOCKS=$[1024*(($(du -m -s "$NATIVE_ROOT" | awk '{print $1}')*12)/10)]
   [ $BLOCKS -lt 4096 ] && BLOCKS=4096
+  IMAGE="$STAGE_DIR/hda.ext2"
 
-  genext2fs -z -D "$WORK/devs" -d "$NATIVE_ROOT" -b $BLOCKS -i 1024 \
-    "$STAGE_DIR/hda.ext2" &&
+  echo "/dev d 755 0 0 - - - - -" > "$WORK/devs" &&
+  echo "/dev/console c 640 0 0 5 1 0 0 -" >> "$WORK/devs" &&
+  genext2fs -z -D "$WORK/devs" -d "$NATIVE_ROOT" -b $BLOCKS -i 1024 "$IMAGE" &&
   rm "$WORK/devs" || dienow
 
   # Extend image size to HDA_MEGS if necessary, keeping it sparse.  (Feeding
   # a larger -b size to genext2fs is insanely slow, and not particularly
   # sparse.)
 
-  echo "$(stat -c %s "$STAGE_DIR/$IMAGE") -lt $SYSIMAGE_HDA_MEGS"
-
   if [ ! -z "$SYSIMAGE_HDA_MEGS" ] &&
-     [ $((`stat -c %s "$STAGE_DIR/$IMAGE"` / (1024*1024) )) -lt "$SYSIMAGE_HDA_MEGS" ]
+     [ $((`stat -c %s "$IMAGE"` / (1024*1024) )) -lt "$SYSIMAGE_HDA_MEGS" ]
   then
     echo resizing image to $SYSIMAGE_HDA_MEGS
-    dd if=/dev/zero of="$STAGE_DIR/$IMAGE" bs=1k count=1 seek=$[1024*1024-1] &&
-    resize2fs "$STAGE_DIR/$IMAGE" ${SYSIMAGE_HDA_MEGS}M || dienow
+    resize2fs "$IMAGE" ${SYSIMAGE_HDA_MEGS}M || dienow
     echo resize complete
   fi
 
