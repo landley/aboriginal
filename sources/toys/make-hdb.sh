bac9456f7b0a05c77292210d2b92a22815bb802d
Rob Landley <rob@landley.net>
2013-07-11 00:17:36 -0500

    Fix typo.

--- sources/toys/make-hdb.sh
+++ sources/toys/make-hdb.sh
@@ -3,7 +3,7 @@ make_hdb()
   # Some distros don't put /sbin:/usr/sbin in the $PATH for non-root users.
   if [ -z "$(which  mke2fs)" ] || [ -z "$(which tune2fs)" ]
   then
-    export PATH=/sbin:/usr/bin:$PATH
+    export PATH=/sbin:/usr/sbin:$PATH
   fi
 
   dd if=/dev/zero of="$HDB" bs=1024 seek=$[$HDBMEGS*1024-1] count=1 &&
